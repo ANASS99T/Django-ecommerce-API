54056016d04b183e87aad78e3f47be53f1ba90a3
Anass Ouled ben tahar <anass.taher@gmail.com>
2024-06-01 16:40:30 +0100

    Category Module with unit tests

--- category/tests.py
+++ category/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+from unittest.mock import patch
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Category
+
+
+class CategoryViewSetTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('categories-list')
+        self.api_client = APIClient()
+        self.data = {
+            'name': 'CategoryTest',
+            'description': 'Category description',
+        }
+
+    @patch('category.views.check_permissions', return_value=True)
+    def test_create_category(self, mock_check_permissions):
+        response = self.api_client.post(self.url, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        new_category = response.data
+        self.assertEqual(new_category['name'], 'CategoryTest')
+        mock_check_permissions.assert_called()
+
+    @patch('category.views.check_permissions', return_value=True)
+    def test_create_sub_category(self, mock_check_permissions):
+        parent_category = Category.objects.create(name='Parent category')
+        self.data['parent'] = parent_category.id
+        response = self.api_client.post(self.url, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        new_category = response.data
+        self.assertEqual(new_category['name'], 'CategoryTest')
+        mock_check_permissions.assert_called()
+
+    @patch('category.views.check_permissions', return_value=True)
+    def test_update_category(self, mock_check_permissions):
+        existing_category = Category.objects.create(name='New category', description='New category description')
+        update_url = reverse('categories-detail', args=[existing_category.id])
+        update_data = {
+            'name': 'TestCategoryUpdated',
+        }
+        response = self.api_client.put(update_url, update_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_category = response.data
+        self.assertEqual(updated_category['name'], 'TestCategoryUpdated')
+        mock_check_permissions.assert_called()
+
+    @patch('category.views.check_permissions', return_value=True)
+    def test_delete_category(self, mock_check_permissions):
+        existing_category = Category.objects.create(name='New category', description='New category description')
+        delete_url = reverse('categories-detail', args=[existing_category.id])
+        response = self.api_client.delete(delete_url)
+        existing_category.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(existing_category.deleted_at is None)
+        mock_check_permissions.assert_called()
+
+    @patch('category.views.check_permissions', return_value=True)
+    def test_list_category(self, mock_check_permissions):
+        Category.objects.create(name='Demo category 1')
+        Category.objects.create(name='Demo category 2')
+        Category.objects.create(name='Demo category 3', parent=Category.objects.first())
+        response = self.api_client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+        mock_check_permissions.assert_called()
+
+    @patch('category.views.check_permissions', return_value=True)
+    def test_retrieve_category(self, mock_check_permissions):
+        existing_category = Category.objects.create(name='New category', description='New category description')
+        retrieve_url = reverse('categories-detail', args=[existing_category.id])
+        response = self.api_client.get(retrieve_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'New category')
+        mock_check_permissions.assert_called()
--- category/views.py
+++ category/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+from helpers.permission_helpers import unauthorized, check_permissions, check_auth
+from category.models import Category
+from category.serializer import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def create(self, request, *args, **kwargs):
+        if not check_permissions(request, 'can_create_category'):
+            return unauthorized()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        if not check_permissions(request, 'can_update_category'):
+            return unauthorized()
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(
+            serializer.data)
+
+
+    def destroy(self, request, *args, **kwargs):
+        if not check_permissions(request, 'can_delete_category'):
+            return unauthorized()
+        role = self.get_object()
+        role.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def list(self, request, *args, **kwargs):
+        if not check_permissions(request, 'can_view_category_list'):
+            return unauthorized()
+        return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        if not check_permissions(request, 'can_view_category'):
+            return unauthorized()
+        return super().retrieve(request, *args, **kwargs)
